45303bdbf546cd94188f056e828f0b585f6534fb
elzup <guild0105@gmail.com>
2021-11-07 21:40:25 +0900

    add ssh-make-key-github.sh

---
+++ ssh-make-key-github.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+name=$1
+[ -z "$name" ] && echo "usage: ./ssh-make-key.sh <key-name>" && exit 1
+file=~/.ssh/${name}
+ssh-keygen -t rsa -N "" -f $file -b 4096
+# chmod 700 ~/.ssh
+# chmod 600 $file
+# chmod 644 $file.pub
+echo -e "\n
+Host github.com\n
+    Hostname github.com\n
+    IdentityFile $file\n
+    User git\n
+" >> ~/.ssh/config
+chmod 600 ~/.ssh/config
