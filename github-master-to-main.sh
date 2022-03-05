2aa1c651d8127eca82b2240abe2af58e9ec22665
elzup <guild0105@gmail.com>
2022-03-05 15:03:22 +0900

    fix github sh

--- github-master-to-main.sh
+++ github-master-to-main.sh
@@ -4,10 +4,14 @@ git branch -m main
 git push origin HEAD -u
 REPO=$(git config --get remote.origin.url |sed -e 's/.*:\(.*\).git/\1/')
 
-echo "open https://github.com/$REPO/settings/branches and"
-echo "change default branch to main"
+ESC_BO="\e[1;m"
+ESC_UL="\e[4;m"
+ESC_END="\e[m"
 
-read -p "git push origin --delete master ? (Y/n)" ANSWER_YN
+echo -e "open ${ESC_UL}https://github.com/$REPO/settings/branches${ESC_END} and"
+echo -e "change default branch from ${ESC_BO}master$ESC_END to ${ESC_BO}main$ESC_END"
+
+read -p "${ESC_BO}git push origin --delete master${ESC_END} ? (Y/n)" ANSWER_YN
 ANSWER_YN=${ANSWER_YN:-"y"}
 
 if [ "${ANSWER_YN}" == "y" ]; then
