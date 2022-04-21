ab8773e8c7dd615993a64e52e806f5bf6d8f0d87
elzup <guild0105@gmail.com>
2022-04-22 00:06:51 +0900

    add safe-run sample

--- zx/safe-run.mjs
+++ zx/safe-run.mjs
@@ -1,15 +1,12 @@
 #!/usr/bin/env zx
 
 const WHITE_LIST = ["red", "green", "blue"];
-const validates = [(color) => WHITE_LIST.includes(color)];
+const validColor = (color) => WHITE_LIST.includes(color);
 
-// whitelist="_1.1.1.1_2.2.2.2_" # split by underber
-//
-// if [ "`echo $whitelist | grep _${ip}_`" ]; then
-//   echo "check IP $ip ok"
-//   echo "$ ${file} ${ip} ${args}"
-//   echo `${file} ${ip} ${args}`
-// else
-//   echo "Error: ip not in white list [${ip}]"
-// fi
-//
+const color = argv._[1];
+
+if (!validColor(color)) {
+  console.error(`Error: color not in list [${color}]`);
+} else {
+  $`echo ${color}`;
+}
