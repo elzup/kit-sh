8dca7e3ca11fb1fdfd9acdaed7df9fb70493ad68
elzup <guild0105@gmail.com>
2023-01-12 10:02:28 +0900

    fix repo path

--- github-master-to-main.sh
+++ github-master-to-main.sh
@@ -2,13 +2,15 @@
 
 git branch -m main
 git push origin HEAD -u
-REPO=$(git config --get remote.origin.url |sed -e 's/.*:\(.*\).git/\1/')
+# REPO=$(git config --get remote.origin.url |sed -e 's/.*:\(.*\).git/\1/')
+REPO_ID=$(git config --get remote.origin.url |sed -e 's/.*github.com.\(.*\).git/\1/')
 
 ESC_BO="\e[1;m"
 ESC_UL="\e[4;m"
 ESC_END="\e[m"
 
-echo -e "open ${ESC_UL}https://github.com/$REPO/settings/branches${ESC_END} and"
+https://github.com///git@github.com/elzup/dentime/settings/branches
+echo -e "open ${ESC_UL}https://github.com/$REPO_ID/settings/branches${ESC_END} and"
 echo -e "change default branch from ${ESC_BO}master$ESC_END to ${ESC_BO}main$ESC_END"
 
 read -p "git push origin --delete master ? (Y/n)" ANSWER_YN
