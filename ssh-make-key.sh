685f4ba6cabce4f9d01f7ed8629f23ac9b068fcd
elzup <guild0105@gmail.com>
2023-07-13 20:53:54 +0900

    update key algos

--- ssh-make-key.sh
+++ ssh-make-key.sh
@@ -2,7 +2,7 @@
 name=$1
 [ -z "$name" ] && echo "usage: ./ssh-make-key.sh <key-name>" && exit 1
 file=~/.ssh/${name}
-ssh-keygen -t rsa -N "" -f $file -b 4096
+ssh-keygen -t ed25519 -C $name -N "" -f $file
 # chmod 700 ~/.ssh
 # chmod 600 $file
 # chmod 644 $file.pub
