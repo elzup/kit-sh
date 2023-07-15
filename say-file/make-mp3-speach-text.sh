8f5dd88522c5b2d9d146e4d9925ac82282379426
elzup <guild0105@gmail.com>
2023-07-16 00:00:06 +0900

    refactor

--- say-file/make-mp3-speach-text.sh
+++ say-file/make-mp3-speach-text.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+OUT_DIR=out
+TMP_FILE=$OUT_DIR/tmp.aiff
+OUT_FILE=$OUT_DIR/voice.mp3
+
 /usr/bin/say -v Kyoko $TEXT
-/usr/bin/say -v Kyoko $TEXT -o out/tmp.aiff
-ffmpeg -i out/tmp.aiff out/voice.mp3
+/usr/bin/say -v Kyoko $TEXT -o $TMP_FILE
+ffmpeg -i $TMP_FILE $OUT_FILE
+rm $TMP_FILE
