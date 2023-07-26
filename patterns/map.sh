5e4cc45f9fc26b84dee94b8d3d006e5124043a2a
elzup <guild0105@gmail.com>
2023-07-26 20:33:56 +0900

    refactor

--- patterns/map.sh
+++ patterns/map.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 function map {
-  local func=$1  # 変換関数を受け取る変数
-  shift          # 最初の引数を削除して残りの引数を取得
+  local func=$1
+  shift
   local transformed=()
   local index=0
 
@@ -16,18 +16,14 @@ function map {
 
 # 変換関数を定義
 function transform {
-  # ここでは要素の長さとインデックスを組み合わせて表示する例とします
   local index=$1
   local item=$2
-  echo "Index: $index, Item: $item, Length: ${#item}"
+  echo "[$index: $item]"
 }
 
 # リスト変数を定義
 fruits=("apple" "orange" "banana" "grape" "kiwi")
 
-function join_by { local IFS="$1"; shift; echo "$*"; }
 # map
-ress=$(map transform "${fruits[@]}")
-
-result=$(join_by "__" "${ress[@]}")
-echo "$result"
+ress=($(map transform "${fruits[@]}"))
+echo $ress
