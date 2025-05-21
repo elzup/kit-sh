2eb5d32d685b2fb082b08391fb072c21370b66f2
elzup <guild0105@gmail.com>
2025-05-21 13:34:22 +0900

    add args

--- proxy.sh
+++ proxy.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+if [ "$#" -gt 1 ]; then
+  echo "Usage: $0 [<target_url>]"
+  exit 1
+fi
+
+TARGET_URL="${1:-http://example.com}"
+
 mitmproxy \
-  --mode reverse:https://www.example.net/ \
+  --mode reverse:"$TARGET_URL" \
   --listen-port 8081 \
   -s proxy-stub.py
