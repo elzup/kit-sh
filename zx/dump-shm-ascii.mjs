d36b25cf5a1ddc7ed2c62b3632dc5115fe6cbeae
elzup <guild0105@gmail.com>
2025-09-16 23:55:19 +0900

    update dump ascii

--- zx/dump-shm-ascii.mjs
+++ zx/dump-shm-ascii.mjs
@@ -1,26 +1,16 @@
 #!/usr/bin/env node
-
 import shm from 'shm-typed-array'
 
-// コマンドライン引数から SHM 名を取得
 const shmName = process.argv[2]
-const len = process.argv[3] || 100
+const len = parseInt(process.argv[3] || '100', 10)
 
-if (!shmName) {
-  console.error('SHM name is required')
-  process.exit(1)
-}
-
-console.log(`SHM name: ${shmName}`)
-
-const buf = shm.get(shmName, 'Buffer')
+const buf = shm.get(shmName, 'Uint8Array') // Bufferより安全
 if (!buf) {
   console.error(`SharedMemory ${shmName} not found`)
   process.exit(1)
 }
 
-const ascii = Buffer.from(buf)
+const ascii = Buffer.from(buf.subarray(0, 16))
   .toString('ascii')
   .replace(/[^\x20-\x7E\n\t]/g, '')
-  .substring(0, len)
 process.stdout.write(ascii)
